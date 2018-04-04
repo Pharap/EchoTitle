495e4a9e97700e69631fed979108b4d59e698b3b
Dreamer2345 <33360259+Dreamer2345@users.noreply.github.com>
2018-04-04 20:16:26 +0100

    Add files via upload

--- ECHOTitle/Title.h
+++ ECHOTitle/Title.h
@@ -1,5 +1,7 @@
 #pragma once
 
+uint8_t Time;
+
 struct Particle{
   uint8_t x;
   uint8_t y;
@@ -9,6 +11,22 @@ struct Particle{
 void Title(){
   Particle Parts[5];
   const short int wind = -1;
+  if (ard.everyXFrames(30))
+    {
+    Time += 1;
+    if (Time == 12) Time = 1;
+    }
+  switch(Time){
+    case 7:ard.drawLine(0,32,0,10); break;
+    case 6:ard.drawLine(0,32,11,14); break;
+    case 5:ard.drawLine(0,32,17,22); break;
+    case 4:ard.drawLine(0,32,23,32); break;
+    case 3:ard.drawLine(0,32,18,42); break;
+    case 2:ard.drawLine(0,32,10,49); break;
+    case 1:ard.drawLine(0,32,0,53); break;
+  }
+
+  
     for (uint8_t i=0;i<5;i++){
       if((!Parts[i].active)&&(random(20) == 0)){
         Parts[i].x = random(125);
